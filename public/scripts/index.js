126a7aa56e7855df1614e8a4eeb68de6fbbb0ab9
max-marschhauser <max.marschhauser@gmail.com>
2022-08-09 15:11:10 +0200

    Fixing bugs
    Fixed bug related to intersection observer and showing text

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,11 +4,11 @@ const copyrightDate = document.querySelector("[data-copyright-date]");
 
 copyrightDate.innerHTML = "&copy;" + new Date().getFullYear() + ".";
 
-//INTERSECTION OBSERVER
+//INTERSECTION OBSERVER CARDS
 
 const cards = document.querySelectorAll("[data-card]");
 
-const observer = new IntersectionObserver(
+const observerCards = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
             entry.target.classList.toggle("section--show", entry.isIntersecting);
@@ -16,11 +16,32 @@ const observer = new IntersectionObserver(
     },
     {
         root: null,
-        threshold: 0.25,
+        threshold: 0.35,
         rootMargin: "0px",
     }
 );
 
 cards.forEach((card) => {
-    observer.observe(card);
+    observerCards.observe(card);
+});
+
+//INTERSECTION OBSERVER TEXT
+
+const text = document.querySelectorAll("[data-text]");
+
+const observerText = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle("text--show", entry.isIntersecting);
+        });
+    },
+    {
+        root: null,
+        threshold: 0.5,
+        rootMargin: "0px",
+    }
+);
+
+text.forEach((text) => {
+    observerText.observe(text);
 });
